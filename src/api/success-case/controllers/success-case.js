4c42bef5f7dd523264479937ee2e63ae5199d24b
maxiPortelBeraud <maximiliano.portel@arbusta.net>
2023-02-01 12:00:26 -0300

    Estructura 2.0 final

--- src/api/success-case/controllers/success-case.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * success-case controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::success-case.success-case');
--- src/api/success-case/services/success-case.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * success-case service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::success-case.success-case');
